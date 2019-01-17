ae7d0ba502908ddb52a3fcff77bf6a924e1fb9b3
Michael Claus <mclausaudio@gmail.com>
2019-01-17 15:04:42 -0800

    creating game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,8 @@
+console.log('connected');
+
+var cardOne = "king";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "queen";
+
+console.log('value of card 1 and 2 is ' + cardOne + " " + cardTwo);
