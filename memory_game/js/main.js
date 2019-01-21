bf5cadf543d73c95cc9bf4f5d74fbd67a5d6ae00
Michael Claus <mclausaudio@gmail.com>
2019-01-21 13:27:55 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,18 +3,24 @@ console.log('connected');
 var cards = ["queen", "queen", "king", "king"]
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-if (cardsInPlay.length === 2) {
+var checkForMatch = function(){
     if (cardsInPlay[0] === cardsInPlay[1]) {
-        alert('You found a match!')
+        console.log('You found a match!');
     } else {
-        alert('sorry try again')
+        console.log('sorry try again');
     }
-    console.log(cardsInPlay);
 }
+
+var flipCard = function(cardId){
+    console.log('user flipped ' + cards[cardId]);
+    cardsInPlay.push(cards[cardId]);
+    if (cardsInPlay.length === 2) {
+        checkForMatch();
+    }
+}
+
+flipCard(3)
+flipCard(2)
+
+
 
