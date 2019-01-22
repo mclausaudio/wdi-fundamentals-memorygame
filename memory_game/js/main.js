17f3a3c967b20cde2fefc9ade947f865ebb2fc9e
Michael Claus <mclausaudio@gmail.com>
2019-01-21 17:45:18 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -25,23 +25,34 @@ var cardsInPlay = [];
 
 var checkForMatch = function(){
     if (cardsInPlay[0] === cardsInPlay[1]) {
-        console.log('You found a match!');
+        alert('You found a match!');
     } else {
-        console.log('sorry try again');
+        alert('sorry try again');
     }
 }
 
-var flipCard = function(cardId){
+var flipCard = function(){
+    var cardId = this.getAttribute('data-id');
     console.log('user flipped ' + cards[cardId].rank);
     console.log(cards[cardId].cardImage);
     cardsInPlay.push(cards[cardId].rank);
+    this.setAttribute('src', cards[cardId].cardImage)
     if (cardsInPlay.length === 2) {
         checkForMatch();
     }
 }
 
-flipCard(3)
-flipCard(2)
+function createBoard(){
+    for (var i = 0; i < cards.length; i++) {
+        var cardElement = document.createElement('img');
+        cardElement.setAttribute('src', 'images/back.png');
+        cardElement.setAttribute('data-id', i);
+        cardElement.addEventListener('click', flipCard)
+        document.getElementById('game-board').appendChild(cardElement);
+    }
+}
+
+createBoard();
 
 
 
